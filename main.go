937d797bd34eee536bdf172f4eef3af8549e5984
Firdavs <firdavs792@gmail.com>
2020-12-22 22:32:15 +0500

    feat: add http.Error function wrapper

--- main.go
+++ main.go
@@ -52,17 +52,16 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 	return func(w http.ResponseWriter, r *http.Request) {
 		query, ok := r.URL.Query()["q"]
 		if !ok || len(query[0]) < 1 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("missing search query in URL params"))
+			http.Error(w, "missing search query in URL params", http.StatusBadRequest)
 			return
 		}
+
 		results := searcher.Search(query[0])
 		buf := &bytes.Buffer{}
 		enc := json.NewEncoder(buf)
 		err := enc.Encode(results)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("encoding failure"))
+			http.Error(w, "encoding failure", http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
